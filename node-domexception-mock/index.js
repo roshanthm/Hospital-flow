a32b8644ad3f0d6c7a77bd5f72da459fb7237e9d
Roshan Thomas <roshanpattathilsep16@gmail.com>
2026-06-15 23:06:28 +0530

    Clear remote repository

--- node-domexception-mock/index.js
+++
@@ -1,7 +0,0 @@
-// Export the native DOMException provided by modern Node.js environments.
-module.exports = globalThis.DOMException || class DOMException extends Error {
-  constructor(message, name) {
-    super(message);
-    this.name = name || 'DOMException';
-  }
-};
